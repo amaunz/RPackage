ecaf768aff6a59bc66fd3383837311b036ad49b0
Andreas Maunz <andreas@maunz.de>
2015-02-20 13:10:44 +0000

    more code for template

---
+++ inst/tests/test-runMain.R
@@ -0,0 +1,9 @@
+context('runMain()')
+
+set.seed(42)
+                               
+# if (file.exists('../../data/N623_input_x.rda')) {
+#   load('../../data/N623_input_x.rda')
+# } else {                       
+#   data(N623_input_x)           
+# } 
--- tests/testthat/test-readDataCsv.R
+++ tests/testthat/test-readDataCsv.R
@@ -1,3 +1,13 @@
 context('readDataCsv')
 
 set.seed(42)
+
+# if (file.exists('../../data/kw.pm.rda')) {                    
+#     load('../../data/kw.pm.rda')           
+# } else {                                     
+#     data(kw.pm)                     
+# }
+
+if (!exists('pkg_globals')) .onLoad()
+assign_pkg_globals('dir', './')
+
